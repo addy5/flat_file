ab116a73ad9adb089601d2cdcf71ed07634b9b3a
Addison Chen <addison.k.chen@gmail.com>
2015-09-25 20:20:36 -0700

    editing all files for posters

--- db/migrate/20150914023511_create_posts.rb
+++ db/migrate/20150914023511_create_posts.rb
@@ -1,12 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :title
-      t.integer :rate
+      t.string :artist
+      t.integer :year
       t.text :profile_url
-      t.text :map_url
-      t.text :body
-      t.string :city
+      t.text :comments
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
