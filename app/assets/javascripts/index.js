e08b07d3d173a0f3bcc4aa364bb9f3f7df85af57
Addison Chen <addison.k.chen@gmail.com>
2015-09-25 20:46:11 -0700

    connected nav links

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -15,4 +15,58 @@ $(document).ready(function() {
 
    $('.notice1').on('click', function(){$(this).slideUp(500);});
 
+
+
+    var posterModule = $('.posterModule');
+    var details = $('.details');
+    var navBar = $('.navBar');
+    var timer;
+
+
+    posterModule.on('mouseenter', function(){
+      var self = $(this);
+
+      if(timer) clearTimeout(timer);
+
+      timer = setTimeout(function(){
+        $('.active').removeClass('active').fadeOut('normal');
+
+        self.children('.details').addClass('active').fadeIn('slow');
+        console.log($(this));
+      },750);
+    });
+
+    var scroll_pos = 0;
+
+    $(document).scroll(function() {
+        scroll_pos = $(this).scrollTop();
+        if(scroll_pos > 80) {
+          navBar.css('background-color','rgba(0,0,0,0.5)');
+          navBar.children().css('opacity', 0.5);
+        } else {
+          navBar.css('background-color','rgba(0,0,0,0.8)');
+          navBar.children().css('opacity', 1);
+        }
+    });
+
+    navBar.on('mouseenter',function(){
+      if(timer) clearTimeout(timer);
+      $('.active').removeClass('active').fadeOut('normal');
+      navBar.css('background-color','rgba(0,0,0,0.8)');
+      navBar.children().css('opacity', 1);
+    });
+
+    navBar.on('mouseleave',function(){
+      var scroll_pos = $(document).scrollTop();
+      if(scroll_pos > 80){
+        navBar.css('background-color','rgba(0,0,0,0.5)');
+        navBar.children().css('opacity', 0.5);
+      }
+    });
+
+    navBar.bind( 'mousewheel DOMMouseScroll', function(e){
+      e.preventDefault();
+    });
+
+
 });
